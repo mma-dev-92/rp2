d2dd2a2f265285ada09c89f5a6efe7a0daa82670
Maciek Mazurek <maciek.mazurek@protonmail.com>
2023-06-04 23:17:07 +0200

    added argument parser for loading config.ini path from cli

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
-from pathlib import Path
+import sys
 
 from calculations import calculate
 from config import load_ini_file
-
+from parser import get_path_from_cli
 
 if __name__ == '__main__':
-    params = load_ini_file(Path('config.ini'))
+    config_ini_path = get_path_from_cli(sys.argv)
+    params = load_ini_file(config_ini_path)
     m, w1 = calculate(params)
 
     print(f" * m = {m}\n * w1 = {w1}")
---
+++ parser.py
@@ -0,0 +1,11 @@
+import argparse
+from pathlib import Path
+
+
+def get_path_from_cli(argv) -> Path:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=Path)
+    args = parser.parse_args()
+    return args.config
+
+
